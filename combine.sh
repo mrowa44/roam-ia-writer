688c0e785a0b750a84f9246dba2b58c11ce90af6
Justyna Rachowicz <mrowa44@gmail.com>
2022-05-20 20:52:07 +0200

    Styles

--- combine.sh
+++ combine.sh
@@ -1,8 +1,15 @@
-cat quattro-theme.css > output.css
-cat colors-quattro.css >> output.css
-cat roam-css-system.css >> output.css
-cat additional-fixes-quattro.css >> output.css
-cat quattro-fonts.css >> output.css
+cat modules/quattro-theme.css > output.css
+cat modules/colors-quattro.css >> output.css
+cat modules/roam-css-system.css >> output.css
+cat modules/additional-fixes-quattro.css >> output.css
+cat modules/quattro-fonts.css >> output.css
 
+cat modules/general.css >> output.css
+cat modules/todos.css >> output.css
+cat modules/focus-mode.css >> output.css
+cat modules/top-bar.css >> output.css
+cat modules/sidebar.css >> output.css
+cat modules/links.css >> output.css
+cat modules/mobile.css >> output.css
 
 npx postcss output.css > output.min.css
