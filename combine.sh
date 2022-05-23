0760aeaae5f062fd97dc8f330abd619a9ac88856
Justyna Rachowicz <mrowa44@gmail.com>
2022-05-23 16:24:44 +0200

    Fonts fixes mostly

--- combine.sh
+++ combine.sh
@@ -5,11 +5,16 @@ cat modules/additional-fixes-quattro.css >> output.css
 cat modules/quattro-fonts.css >> output.css
 
 cat modules/general.css >> output.css
+cat modules/text.css >> output.css
 cat modules/todos.css >> output.css
 cat modules/focus-mode.css >> output.css
 cat modules/top-bar.css >> output.css
 cat modules/sidebar.css >> output.css
 cat modules/links.css >> output.css
 cat modules/mobile.css >> output.css
+
+cat scripts/autofocus.js > script.js
+echo '\n\n\n\n' >> script.js
+cat scripts/scroll.js >> script.js
 
 npx postcss output.css > output.min.css
---
+++ scripts/scroll.js
@@ -0,0 +1,11 @@
+// Turn off focus mode on scroll
+
+const scrollHandler = (event) => {
+    if (document.activeElement && document.activeElement !== document.documentElement) {
+        document.documentElement.click();
+    }
+}
+
+window.addEventListener('wheel', scrollHandler, {passive: true});
+window.addEventListener('touchmove', scrollHandler, {passive: true});
+
